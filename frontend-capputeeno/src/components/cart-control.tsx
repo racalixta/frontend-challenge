376c861080b268e5a5f57e886cc6e07d1c362742
racalixta <ra.calixta@gmail.com>
2023-09-12 22:06:32 -0300

    feat: creating header

---
+++ frontend-capputeeno/src/components/cart-control.tsx
@@ -0,0 +1,29 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { CartIcon } from "./icons/cart-icon";
+import styled from "styled-components";
+
+const CartCount = styled.span`
+  width: 17px;
+  height: 17px;
+  border-radius: 100%;
+  padding: 0 5px;
+  font-size: 11px;
+  background-color: var(--delete-color);
+  color: white;
+  margin-left: -10px;
+`
+
+const Container = styled.div`
+  position: relative;
+`
+
+export function CartControl() {
+  const { value } = useLocalStorage('cart-items', []);
+
+  return (
+    <div>
+      <CartIcon />
+      {value.length && <CartCount>{value.length}</CartCount>}
+    </div>
+  )
+}
